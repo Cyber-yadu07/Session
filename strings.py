db04a0bd78dc9f5923bdfba67968857cfd167f7d
E V I R A L <74889213+Teameviral@users.noreply.github.com>
2021-12-02 22:21:26 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,52 @@
+GIVE_API = """Hey!!! Let's Start Generating Your Telethon StringSession 😁😁.
+Get API ID and API HASH from @apiscrapperbot or my.telegram.org.
+
+Use /cancel to Cancel Process Anytime
+
+Now, Send API ID
+    👇Below👇"""
+GIVE_HASH = """Now, Send API HASH
+    👇Below👇"""
+PHONE = """Now, Your Phone Number with Country code
+    👇Below👇"""
+PHONE_AGAIN = """❌ **ERROR** ❌
+
+Send Phone Number Again."""
+PHONE_WRONG = """❌ **WRONG PHONE** ❌
+
+/start Process Again."""
+CODE = """Send Telegarm Login Code Which you recieved.
+
+**Note - Put a Space Between Numbers**
+Like - if code is `12345` , write `123 45`"""
+CODE_AGAIN = """❌**INVALID CODE**❌
+
+Send Code Again."""
+CODE_WRONG = """❌**INVALID CODE**❌
+
+Use /start To Start Process Again."""
+PASS = """Enter Your 2nd Verification Password Of Telegram.
+    👇Below👇"""
+PASS_AGAIN = """❌**INCORRECT PASSWORD**❌
+
+Send Password Again"""
+PASS_WRONG = """❌**INCORRECT PASSWORD**❌
+
+Use /start To Start Process Again."""
+SESSION = """**This Is Your Telethon Userbot StringSession**
+⚠️**DO NOT SHARE WITH ANYONE**⚠️
+
+`{}`
+
+__Regards - @TeamUltroid__"""
+PYSESSION = """**This Is Your Pyrogram Vcbot StringSession**
+⚠️**DO NOT SHARE WITH ANYONE**⚠️
+
+`{}`
+
+__Regards - @TeamUltroid__"""
+FLOOD = "Floodwait 😅😅, Try again After `{}` seconds"
+WRONG = """Something Went Wrong
+Try again later😅."""
+ABORT = """**Process Aborted**"""
+EXPIRED = "**Expired Code!!!**\n/start to start again."
